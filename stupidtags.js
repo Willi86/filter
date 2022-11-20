bf31c22a788da8e6468d72f09c0d693e2891d640
Willi86 <williamlundqvist@yahoo.com>
2022-11-20 12:05:13 +0100

    Selected tag is shown

--- stupidtags.js
+++ stupidtags.js
@@ -37,7 +37,7 @@ function w3RemoveClass(element, name) {
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("btn-container");
 var btn = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
+for (var i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
